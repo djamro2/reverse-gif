19e65b36c1a3007e2c39761eac62b960b3ffd6cd
Daniel Jamrozik <djamro2@illinois.edu>
2015-09-27 10:24:36 -0500

    working reverse function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # reverse-gif
+
+# Includes a function as input which will take in a gif name and using imagemagick, it will
+# return the reversed version of it
--- reverse.js
+++ reverse.js
@@ -1,61 +1,27 @@
-//what does this file accomplish?
-//file named in here that I will reverse
-//if file doesn't exist, output message
-//if output file exists, replace output
 
-//will use one command to split up gif into frames (my own python program)
-//another command to put those images together (pygifme)
+//includes a function called reverseGif which will return a reversed gif in the same folder as the file running it
 
 var fs = require('fs');
-var sys = require('sys');
 var exec = require('child_process').exec;
 var child;
 
-//the gif I will reverse
-var gifName = 'funny.gif';
-var path = 'output';
+module.exports.reverseGif = function(gifName) {
 
-var deleteFolderRecursive = function(path) {
-  if( fs.existsSync(path) ) {
-    fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
-      if(fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
+  //file has to exist to run
+  if (!fs.existsSync(gifName)){
+  	console.log('Could not find ' + gifName + ' Will stop current node process' );
+  	process.exit();
   }
-};
 
-var makeDirectory = function(_path){
-	if (!fs.existsSync(_path)){
-    	fs.mkdirSync(_path);
-	}
-};
+  var command = " convert " + gifName + " -coalesce -reverse \
+          -quiet -layers OptimizePlus  -loop 0 " + gifName.split('.')[0] + "-reverse.gif";
 
-//EXECUTING CODE BELOW
+  child = exec(command, function (error, stdout, stderr) {
+  	console.log(stdout);
+  	console.log(stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
 
-//file has to exist to run
-if (!fs.existsSync(gifName)){
-	console.log('Could not find ' + gifName + ' Will stop current node process' );
-	process.exit();
 }
-
-//create blank output folder after these two lines
-deleteFolderRecursive(path);
-makeDirectory(path);
-
-
-//run the python program which will reverse gif (takes gif name as an argument)
-
-var command = "python reverse.py " + gifName;
-
-child = exec(command, function (error, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
